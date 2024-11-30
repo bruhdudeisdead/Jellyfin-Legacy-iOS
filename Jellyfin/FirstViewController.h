de1bf068050105337f26a5207a856025b421fc97
Jack <a1429_fan@DangleBook-Pro.local>
2024-11-30 07:51:39 -0600

    Initial Commit

---
+++ Jellyfin/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Jellyfin
+//
+//  Created by Jack on 11/30/24.
+//  Copyright (c) 2024 DumbStupidStuff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ Jellyfin/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Jellyfin
+//
+//  Created by Jack on 11/30/24.
+//  Copyright (c) 2024 DumbStupidStuff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
